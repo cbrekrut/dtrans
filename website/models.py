4d87590da6f818d7dbca73ec513e52a19a27981d
WazowskiMike <wazowskipuffs@yandex.ru>
2024-11-11 22:55:22 +0300

    finished

--- website/models.py
+++ website/models.py
@@ -22,8 +22,6 @@ class BlogImage(models.Model):
     blog = models.ForeignKey(Blog, related_name='images', on_delete=models.CASCADE) 
     image = models.ImageField(upload_to='blog/')
 
-    def __str__(self):
-        return f"Image for {self.article.title}"
 
 class Service(models.Model):
     name = models.CharField(max_length=200, unique=True)
--- website/views.py
+++ website/views.py
@@ -4,20 +4,29 @@ import requests
 from .models import Service, Blog
 
 def index(request):
-    return render(request, 'index.html')
+    services = Service.objects.all()
+    blogs = Blog.objects.all()
+    return render(request, 'index.html',{'services':services, 'blogs':blogs})
 def about(request):
     return render(request, 'about.html')
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        telephone = request.POST.get('telephone')
     return render(request, 'contacts.html')
 def blog(request):
-    blogs = BLog.objects.all()
-    return render(request, 'blog.html',{'blogs':blog})
+    blogs = Blog.objects.all()
+    return render(request, 'blog.html',{'blogs':blogs})
 def blog_detail(request, slug):
+    blogs = Blog.objects.all()
     blog = get_object_or_404(Blog, slug=slug)
-    return render(request, 'blog_detail.html',{'blog':blog})
+    return render(request, 'blog_detail.html',{'blog':blog, 'blogs': blogs})
 def services(request):
     services = Service.objects.all()
-    return render(request, 'services.html',{'services':services})
+    page_obj = Service.objects.all()
+    return render(request, 'services.html',{'services':services, 'page_obj': page_obj})
 def service_detail(request, slug):
+    services = Service.objects.all()
     service = get_object_or_404(Service, slug=slug)
-    return render(request, 'service_detail.html',{'service':service})
+    return render(request, 'service_detail.html',{'service':service, 'services': services})
+
